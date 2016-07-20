428cb0e48a6592a22f4fdb6be812dbd8268cd4fd
Manu-chroma <manvendra0310@gmail.com>
2016-07-20 21:08:01 +0530

    added template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double pi = 3.14159265358979323846;
+const int INF = ;
+
+typedef long long int li;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
